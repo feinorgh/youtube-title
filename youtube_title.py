d06149a59246c28d2fe056f224bc210848e47b97
Pär Karlsson <feinorgh@gmail.com>
2015-05-12 22:10:21 +0200

    Make IRC bot
    Change name of youtube-title to youtube_title.py for use as module.

--- youtube_title.py
+++ youtube_title.py
@@ -29,12 +29,8 @@ def parse_iso8601_duration(duration):
             parts.append("%s %s" % (d[span], span))
     return ", ".join(parts)
 
-if __name__ == "__main__":
-    if ( sys.argv.__len__() < 2 ):
-        print("Please provide a YouTube URL")
-        exit(1)
 
-    url     = sys.argv[1]
+def get_video_title(url):
     youtube = build("youtube", "v3", developerKey = get_api_key())
     re_id   = re.compile("v=(\w+)")
     video_id = re_id.search(url).group(1)
@@ -50,4 +46,16 @@ if __name__ == "__main__":
 
     video = videos_list["items"][0]
     duration = parse_iso8601_duration(video["contentDetails"]["duration"])
-    print("%s (%s)" % (video["snippet"]["title"], duration))
+    output = video["snippet"]["title"] + " (" + duration + ")"
+    return output
+
+
+if __name__ == "__main__":
+    if ( sys.argv.__len__() < 2 ):
+        print("Please provide a YouTube URL")
+        exit(1)
+
+    url     = sys.argv[1]
+    if url:
+        print get_video_title(url)
+
